6a4dc8528114f4a0b014e56960efcd86568384f4
Ara Software <71103842+cr4ck3r1@users.noreply.github.com>
2021-02-07 17:30:30 +0300

    Update soon.py

--- soon.py
+++ soon.py
@@ -19,7 +19,6 @@ def login():
 	else:
 		print("Xalata Tkaya Nama bo punjabi bnera")
 		time.sleep(3)
-                os.system("xdg-open https://instagram.com/i.punjabii")
 		os.system("clear")
 		login()
 login()
